9e6a36d9264498b1b74a50551f61ffad064fea47
Saurabh Kumar <saurabhk15@outlook.com>
2020-07-25 08:12:18 +0530

    init

---
+++ src/main/java/com/saurabh/mongodblegostore/MongoDbLegoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.mongodblegostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbLegoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbLegoStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/mongodblegostore/MongoDbLegoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.mongodblegostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbLegoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
